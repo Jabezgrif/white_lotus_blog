f4ce411d0e05627f2851ef2d9ed022321c8b7c81
Daniel Griffith <danielgrif16@gmail.com>
2025-11-05 22:47:38 +0800

    chore: update firebaseConfig to use environment variables for configuration

--- src/lib/firebaseConfig.js
+++ src/lib/firebaseConfig.js
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth';
 import {getMessaging} from "firebase/messaging";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD5mxOD_hZg-alNRF5dWy81kVHfiO2bmIU",
-    authDomain: "white-lotus-blog.firebaseapp.com",
-    projectId: "white-lotus-blog",
-    storageBucket: "white-lotus-blog.firebasestorage.app",
-    messagingSenderId: "863315670713",
-    appId: "1:863315670713:web:d805535333e31030aa4d68",
-    measurementId: "G-2H85315QSC"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
